74371af7fd5d765991b65f6da8e01928f5ce15a6
devloop0 <nathreya.master@gmail.com>
2016-08-17 17:15:10 -0700

    Added header to include everything from midnight

---
+++ midnight.hpp
@@ -0,0 +1,29 @@
+#include "algs.hpp"
+#include "type_list.hpp"
+#include "numeric_operators.hpp"
+#include "bool_operators.hpp"
+#include "halgs.hpp"
+#include "htype_list.hpp"
+#include "math.hpp"
+#include "ratio.hpp"
+#include "ratio_numeric_operators.hpp"
+#include "ratio_bool_operators.hpp"
+#include "core_interop.hpp"
+#include "interop.hpp"
+#include "core_types.hpp"
+#include "generic_operators.hpp"
+#include "ratio_operators.hpp"
+#include "conditional_operator.hpp"
+#include "placeholders.hpp"
+
+using namespace midnight::core;
+using namespace midnight::operators;
+using namespace midnight::list;
+using namespace midnight::algs;
+using namespace midnight::hlist;
+using namespace midnight::halgs;
+using namespace midnight::interop;
+using namespace midnight::math;
+using namespace midnight::math::ratio;
+using namespace midnight::math::ratio::operators;
+using namespace midnight::placeholders;
